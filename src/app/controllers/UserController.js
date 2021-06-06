c714be4c1a8dd191498fc87d0cadd90b9169c0c9
Lucas Zanin <lukinhas.so.lokura@hotmail.com>
2021-06-06 12:49:02 -0300

    Declaration Of UserController methods and routes

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,26 @@
+const { model } = require("mongoose");
+
+class UserController{
+     create(req,res){
+
+     }
+
+    index(req,res){
+        res.json({msg: 'test'});
+    
+    }
+    show(req,res){
+        
+    
+    }
+    update(req,res){
+        
+    
+    }
+    delete(req,res){
+        
+    
+    }
+}
+
+modele.exports= new UserController();/*Cria um controlador e exporta a palavra New por ser uma classe é utilizada para instanciar o objeto */
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,11 @@
 const routes = require('express').Router();/* Importa o express criando uma constante para isso*/ 
+const UserController= require('./app/controllers/UserController')
 
-routes.get('/usuarios',(req,res)=>{
- res.json({msg: 'usuários'});/* Função de call back , para teste da rota*/ 
-})
-
-
-routes.post('/usuarios',(req,res)=>{
-    const nome=req.body.nome;
-    const idade=req.body.idade;
-    res.json({msg: `O Nome é ${nome}/ A idade é ${idade}`});/* Função de call back , para teste da rota*/ 
-
-})
+/*Rotas para acessar o usuário */
+routes.get('/users',UserController.index);  /* Mostra todos os usuários */
+routes.get('/users/:_id',UserController.show);/* Mostra um usuário */
+routes.post('/users',UserController.create);/* Cria um usuário */
+routes.put('/users/:_id',UserController.update);/* Atualiza um usuário */
+routes.delete('/users/:_id',UserController.delete);/* Deleta um usuário */
 
 module.exports=routes;/* Exporta o routes para fora para que o app.js utilize a rota*/ 
