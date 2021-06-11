2d6e5bd84a2f6eb53c105698a43bbdb3fd95429a
Lucas Zanin <lukinhas.so.lokura@hotmail.com>
2021-06-11 20:36:53 -0300

    feat add logic usercontroller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,26 +1,62 @@
-const { model } = require("mongoose");
+const User= require('../models/User');
+
 
 class UserController{
-     create(req,res){
+     async create(req, res){  /*Cria um usuário no banco */
+        const {firstname,lastname,age,email,address}= req.body; /*Passa os atributos para o cadastro do banco */
+       try {
+        const creadetUser = await User.create({firstname,lastname,age,email,address});
+        
+        return res.status(200).json(creadetUser); /*Retornar Status Ok*/
+    } catch(err) {
+       return res.status(400).json(err);/*Retornar Erro caso não consiga cadastrar*/
 
-     }
+    }
+    
+    }
 
-    index(req,res){
-        res.json({msg: 'test'});
-    
+    async index(req, res){ /*Mostra todos os usuários cadastrados no banco */
+        try {
+           const users = await User.find();/* Utiliza a Função Find para mostrar os cadastros do banco */ 
+            
+            return res.status(200).json(users);
+        } catch(err){
+           return res.status(400).json(err);
+        }
+
     }
-    show(req,res){
-        
+    async show(req, res){
+        const{_id}=req.params;
+         try{
+               const user = await User.findOne({_id});
+               return res.status(200).json(user);
+            }catch(err) {
+               return res.status(400).json(err);
+          }     
+            
+         }
     
+    async update(req ,res){
+      const {_id } =req.params;
+
+      try{
+            const updated = await User.update({id},req.body).exec();
+            return res.status(200).json(updated);
+         }catch(err){
+            return res.status(400).json(err);
+         }
+   
     }
-    update(req,res){
-        
-    
-    }
-    delete(req,res){
-        
+    async delete(req, res){
+      const{_id}=req.params;
+      try{
+         const deleted = await User.deleteOne({_id});
+         return res.status(200).json(deleted);
+      }catch(err){
+         return res.status(400);
+      }
     
     }
 }
 
-modele.exports= new UserController();/*Cria um controlador e exporta a palavra New por ser uma classe é utilizada para instanciar o objeto */
+module.exports= new UserController();/*Cria um controlador e exporta a palavra New por ser uma classe é utilizada para instanciar o objeto */
